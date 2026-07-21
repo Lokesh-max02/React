39ddb3b0a176e7b6632d8388e87d57ef4c2c1fbd
lokeshj <lokeshj022004@gmail.com>
2026-07-21 23:25:12 +0530

    updated

---
+++ Day15/UseEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Side_Effect from './component/Side_Effect'
+
+const App = () => {
+  return (
+    <>
+    <div className='p-4'>
+      <Side_Effect/>
+    </div>
+    </>
+  )
+}
+
+export default App
